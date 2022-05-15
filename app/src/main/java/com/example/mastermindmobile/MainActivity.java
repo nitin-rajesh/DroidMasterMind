05255980d1f6ca363ed46f058c8f5f87ff2fe679
IT Guy <nitinkrajesh@gmail.com>
2022-05-15 22:43:44 +0530

    MM Mobile revision

--- app/src/main/java/com/example/mastermindmobile/MainActivity.java
+++ app/src/main/java/com/example/mastermindmobile/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.mastermindmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    SharedPreferences gameSettings;
+    SharedPreferences.Editor gameSettingsEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameSettings = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        gameSettingsEditor = gameSettings.edit();
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +40,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.config_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.grid4:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void openGameScreen(){
         Intent intent = new Intent(this,GameActivity.class);
         startActivity(intent);
     }
-
 }
